57d1d85f6de13176ecbed207f891a9041a46d22a
Muhammad Maudhodi Fikri <fikrigg339@gmail.com>
2026-04-15 17:58:07 +0800

    menambahkan animasi serang

--- player.gd
+++ player.gd
@@ -2,11 +2,22 @@ extends CharacterBody2D
 
 const kecepatan = 100
 var arah = "diam"
+var sedang_serang = false
 
 func _physics_process(delta):
 	gerak_player(delta)
 
 func gerak_player(delta):
+	if Input.is_action_just_pressed("klik_kiri") and not sedang_serang:
+		serang()
+		return
+
+	if sedang_serang:
+		velocity.x = 0
+		velocity.y = 0
+		move_and_slide()
+		return
+
 	if Input.is_action_pressed("ui_right"):
 		arah = "kanan"
 		arah_player(true)
@@ -31,32 +42,61 @@ func gerak_player(delta):
 		arah_player(false)
 		velocity.x = 0
 		velocity.y = 0
-		
+
 	move_and_slide()
-		
-func arah_player(gerak):
-	var arah_sekarang = arah
+
+func serang():
+	sedang_serang = true
+	velocity.x = 0
+	velocity.y = 0
+
 	var animasi = $AnimatedSprite2D
-	
-	if arah_sekarang == "kanan":
+
+	# Tentukan animasi serang sesuai arah
+	if arah == "kanan":
+		animasi.flip_h = false
+		animasi.play("serang")
+	elif arah == "kiri":
+		animasi.flip_h = true
+		animasi.play("serang")
+	elif arah == "atas":
+		animasi.flip_h = false
+		animasi.play("serang_atas")
+	elif arah == "bawah":
+		animasi.flip_h = false
+		animasi.play("serang_bawah")
+	else:
+		animasi.flip_h = false
+		animasi.play("serang")
+
+	# Tunggu animasi selesai baru lanjut
+	await animasi.animation_finished
+
+	sedang_serang = false
+	animasi.play("diam")
+
+func arah_player(gerak):
+	var animasi = $AnimatedSprite2D
+
+	if arah == "kanan":
 		animasi.flip_h = false
 		if gerak:
 			animasi.play("jalan_kanan")
 		else:
 			animasi.play("diam")
-	elif  arah_sekarang == "kiri":
+	elif arah == "kiri":
 		animasi.flip_h = true
 		if gerak:
 			animasi.play("jalan_kiri")
 		else:
 			animasi.play("diam")
-	elif  arah_sekarang == "atas":
+	elif arah == "atas":
 		animasi.flip_h = false
 		if gerak:
 			animasi.play("jalan_atas")
 		else:
 			animasi.play("diam")
-	elif  arah_sekarang == "bawah":
+	elif arah == "bawah":
 		animasi.flip_h = false
 		if gerak:
 			animasi.play("jalan_bawah")
